ac0681bb60d87ec55767b144e0e41881e58ac1d2
Joao Dias <jpgd94@poli.ufrj.br>
2022-10-21 18:45:04 -0300

    'fix" resolvendo a questãod e adicionar novas task

--- src/components/NewTask.tsx
+++ src/components/NewTask.tsx
@@ -3,38 +3,58 @@ import { PlusCircle } from 'phosphor-react'
 import { Tasks } from './Tasks'
 
 import styles from './NewTask.module.css';
+import { useState } from 'react';
 
-const tasks = [
-    {
-        id: 1,
-        content: "Comprar arroz",
-        isCompleted: false,
-    },
-    {
-        id: 2,
-        content: "Comprar Feijão",
-        isCompleted: false,
-    },
-    {
-        id: 3,
-        content: "Ir na Academia",
-        isCompleted: false,
+interface TaskProps {
+    tasks: {
+        content:string;
+        isCompleted:boolean;
     }
-    ]
+}
+
+
+
 export function NewTask(){
 
-    return (
-        <div className={styles.newTask} >
-            <input placeholder='Adicione uma Tarefa'>
-            </input>
-            <button type='submit'>
-                    Clicar
-                    <PlusCircle size={16} /> 
-            </button>
+    const [tasks, setTasks] = useState([ 
+        'Comprar Feijão'
+                 
+    ])
 
+    function handleCreateNewTask(){
+        event?.preventDefault()
+        console.log('oi')
+        const NewTaskText = event.target.text.value
+        console.log(NewTaskText)
+        setTasks([...tasks, NewTaskText])
+                
+
+       
+    }
+
+    return (
+        <div className={styles.tasksBox}>
+
+            <form onSubmit={handleCreateNewTask}>
+                <div className={styles.newTask} >
+                    <input name="text" placeholder='Adicione uma Tarefa'>
+                    </input>
+                    <button type='submit'>
+                            Clicar
+                            <PlusCircle size={16} /> 
+                    </button>
+
+                </div>
+            </form>
+            <div className={styles.statusTasks}>
+                <span>Tarefas Criadas</span> 
+                        
+                <span>Tarefas Concluidas</span>
+
+            </div>
             <div className={styles.tasksCreated}>
                 {tasks.map(task => {
-                    return <Tasks content={task.content} />
+                    return <Tasks key={task} content={task} />
                 })}
                 
 
